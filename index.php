345234341721338b7534c8345f36ffa5be5f74e4
Dagmar Cirino <dagmar.mv@gmail.com>
2018-07-26 01:03:39 -0300

    Iniciand o index
    Só coloquei umas coisas que pretendo fazer com este site.

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
 <h1>Olá Mundo! Td mundo ligado?</h1>
+<br>
+<ul>
+  <li>
+    Artigos de Dev e bobagens.
+  </li>
+  <li>
+    Lista de Grupos BR.
+  </li>
+  <li>
+    Exemplos de códigos.
+  </li>
+  <li>
+    Lista de Eventos e Colaboração.
+  </li>
+  <li>
+    Também vai ter lista de games, com preços e variações em lojas diversas.<br>
+    Vou usar isso para testar vários métodos de crawler.
+  </li>
+</ul>
+<hr>
+<p>
+  É isso.<br>
+  Qualquer coisa, manda um salve lá no e-mail do Gmail: <b>dagmar.mv</b>
+</p>
